1aee1cdfbb6aaa74831d298d3b0a8877bcc17908
Ahmed Ibrahim <siddiqui.ahmedibrahim@gmail.com>
2022-12-25 19:40:11 +0500

    initial

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use Auth;
+use Exception;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+       public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+       public function handleCallback()
+    {
+        try {
+     
+            $user = Socialite::driver('google')->user();
+      
+            $finduser = User::where('social_id', $user->id)->first();
+      
+            if($finduser){
+      
+                Auth::login($finduser);
+     
+                return redirect('/home');
+      
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'social_id'=> $user->id,
+                    'social_type'=> 'google',
+                    'password' => encrypt('testingpassword')
+                ]);
+     
+                Auth::login($newUser);
+      
+                return redirect('/home');
+            }
+     
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+            public function login(Request $request)
+    {
+        $user=\Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password]);
+
+        if ($user) 
+        {
+             
+        return redirect(url('/dashboard'))->with('success' , 'Login Successfully');             
+        }
+        else{
+
+            return redirect(url('/login'))->with('error',"Invalid Username And Password");
+
+        }
+
+    }
+}
